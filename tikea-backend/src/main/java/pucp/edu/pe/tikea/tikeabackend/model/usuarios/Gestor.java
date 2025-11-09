af8efa8ba6d27418a02c0a2ca1c012d4f088b444
Sandy <a20201230@pucp.edu.pe>
2025-11-09 18:09:42 -0500

    Completado de la capa model

--- tikea-backend/src/main/java/pucp/edu/pe/tikea/tikeabackend/model/usuarios/Gestor.java
+++ tikea-backend/src/main/java/pucp/edu/pe/tikea/tikeabackend/model/usuarios/Gestor.java
@@ -1,4 +1,22 @@
 package pucp.edu.pe.tikea.tikeabackend.model.usuarios;
 
-public class Gestor {
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.CreationTimestamp;
+import pucp.edu.pe.tikea.tikeabackend.model.Usuario;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Gestor")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@PrimaryKeyJoinColumn(name = "id")
+@Getter
+@Setter
+public class Gestor extends Usuario {
+    @Column(name = "areaGestion")
+    private TipoArea areaGestion;
 }
--- tikea-backend/src/main/java/pucp/edu/pe/tikea/tikeabackend/model/usuarios/Productor.java
+++ tikea-backend/src/main/java/pucp/edu/pe/tikea/tikeabackend/model/usuarios/Productor.java
@@ -1,4 +1,36 @@
 package pucp.edu.pe.tikea.tikeabackend.model.usuarios;
 
-public class Productor {
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.CreationTimestamp;
+import pucp.edu.pe.tikea.tikeabackend.model.Usuario;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Productor")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@PrimaryKeyJoinColumn(name = "id")
+@Getter
+@Setter
+public class Productor extends Usuario {
+    @ManyToOne
+    @JoinColumn(name = "idGestor")
+    private Gestor gestor;
+    private String razonSocial;
+    private String RUC;
+    private String direccionFisica;
+    @Column(name = "TipoEstadoProductor")
+    private TipoEstadoProductor estadoProductor;
+    @Column(name = "documentacionAdjunta")
+    @Lob
+    private byte[] documentacionFisica;
+    private int localesRegistrados;
+    private int eventosRegistrados;
+    private int promocionesCreadas;
+    @CreationTimestamp
+    private LocalDateTime fechaVerificacion;
 }
